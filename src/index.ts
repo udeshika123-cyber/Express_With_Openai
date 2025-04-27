3908ae09ada77765e5accbf914459f4945ec34b1
udeshika123-cyber <udeshikaprabhashini21@gmail.com>
2025-04-27 14:55:56 +0530

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,16 @@
 
 import express, { Request, Response } from 'express';
 import { Routes } from './routes';
+import { OpenAiService } from './service';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const openAi=OpenAiService.getInstance();
 
 app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+  res.send ('Hellow world');
 })
 
 app.use('/api', new Routes().router);
